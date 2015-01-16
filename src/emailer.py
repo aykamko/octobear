5cd7c430838b10698082f554df2bafa0699185f9
Austin Chen <akrolsmir@gmail.com>
2015-01-16 00:58:03 -0800

    Move email strings to config

--- src/emailer.py
+++ src/emailer.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from jinja2 import Environment, PackageLoader
+from . import config
 
 def send_plaintext(to_address, subject, plaintext):
     # TODO: @austin
@@ -13,16 +14,11 @@ def send_html(to_address, subject, html_string):
     pass
 
 def send(user, subject, template):
-    # me == my email address
-    # you == recipient's email address
-    me = "cs61b@sandboxe9b32d5651b3403c820ae7c5d2cd3926.mailgun.org"
-    you = user[u'email']
-
     # Create message container - the correct MIME type is multipart/alternative.
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
-    msg['From'] = me
-    msg['To'] = you
+    msg['From'] = config['email_from']
+    msg['To'] = user[u'email']
 
     # Create the body of the message (a plain-text and an HTML version).
     text = "HTML failed to render =(.\nPlease let us know about this!"
@@ -43,14 +39,14 @@ def send(user, subject, template):
     msg.attach(part2)
 
     # Credentials (if needed)
-    username = 'postmaster@sandboxe9b32d5651b3403c820ae7c5d2cd3926.mailgun.org'
-    password = 'e581750b7b1dfe506b85398c747c6fc6'
+    username = config['email_username']
+    password = config['email_password']
 
     # The actual mail send
-    s = smtplib.SMTP('smtp.mailgun.org', 587)
+    s = smtplib.SMTP(config['smtp_host'], int(config['smtp_port']))
     s.starttls()
     s.login(username,password)
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
-    s.sendmail(me, you, msg.as_string())
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
     s.quit()
