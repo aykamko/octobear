2951f546fb56e4c6189dfa473e82aa200a931a73
Aleks Kamko <aykamko@gmail.com>
2015-01-19 20:50:59 -0800

    small change; too tired to notice

--- src/app.py
+++ src/app.py
@@ -7,8 +7,7 @@ from .ag_result_server import run_ag_result_server
 from .queue_worker import start as start_queue_worker
 
 def run(no_account_forms=False):
-    if no_account_forms:
-        config['no_account_forms'] = True
+    config['no_account_forms'] = bool(no_account_forms)
     rserver = run_registration_server(port=int(config['registration_server_port']))
     agserver = run_ag_result_server(port=int(config['ag_result_server_port']))
     start_queue_worker()
